01a84e20abce5b100af36c200e242c2ad28cebe6
Remy Kooistra <remy.kooistra.km@gmail.com>
2018-06-07 11:52:58 +0200

    added test to see if the computerDecorator is a Computer

---
+++ app/Classes/ComputerDecorator.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Classes;
+
+use App\Classes\Contracts\Computer;
+
+class ComputerDecorator extends PlainComputer implements Computer
+{
+
+}
---
+++ tests/Unit/Unit_ComputerDecoratorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Classes\ComputerDecorator;
+use App\Classes\Contracts\Computer;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Unit_ComputerDecoratorTest extends TestCase
+{
+    protected $sut;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->sut = new ComputerDecorator();
+    }
+
+    public function testInstantiation()
+    {
+       $this->assertNotNull($this->sut);
+    }
+
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Computer::class, $this->sut);
+    }
+}
