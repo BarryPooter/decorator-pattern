6f5e99e30d4ed230127b3a650ed2754fc701f33f
Remy Kooistra <remy.kooistra.km@gmail.com>
2018-06-07 12:38:42 +0200

    Test if the ComputerFloppyReader is an instance of Computer

--- app/Classes/ComputerFloppyReader.php
+++ app/Classes/ComputerFloppyReader.php
@@ -2,7 +2,17 @@
 
 namespace App\Classes;
 
-class ComputerFloppyReader
-{
+use App\Classes\Contracts\Computer;
 
+class ComputerFloppyReader implements Computer
+{
+    public function getParts(): string
+    {
+        // TODO: Implement getParts() method.
+    }
+
+    public function getCost(): float
+    {
+        // TODO: Implement getCost() method.
+    }
 }
--- tests/Unit/Unit_ComputerFloppyReaderTest.php
+++ tests/Unit/Unit_ComputerFloppyReaderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Classes\Contracts\Computer;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -19,5 +20,10 @@ class Unit_ComputerFloppyReaderTest extends TestCase
     public function testInstantiation ()
     {
         $this->assertNotNull($this->sut);
+    }
+
+    public function testInstance ()
+    {
+        $this->assertInstanceOf(Computer::class, $this->sut);
     }
 }
