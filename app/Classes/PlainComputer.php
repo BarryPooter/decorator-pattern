b9c6b5d79a9393720c13f638e43a84069a7299b5
Remy Kooistra <remy.kooistra.km@gmail.com>
2018-06-07 11:38:03 +0200

    The way to get parts is now refactored

--- app/Classes/PlainComputer.php
+++ app/Classes/PlainComputer.php
@@ -6,9 +6,17 @@ use \App\Classes\Contracts\Computer;
 
 class PlainComputer implements Computer
 {
+    protected $parts;
+
+    public function __construct()
+    {
+        $this->parts = (array) null;
+        $this->parts[] = 'case';
+    }
+
     public function getParts(): string
     {
-        return (string) null;
+        return (string) implode(',', $this->parts);
     }
 
     public function getCost(): float
